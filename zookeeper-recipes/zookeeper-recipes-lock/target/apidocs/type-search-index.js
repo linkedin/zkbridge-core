4aa8eb625c7ec598b403a9113ddfbf5b9578350f
Komal Desai <kdesai@linkedin.com>
2023-10-31 11:04:55 -0700

    Update the Spiral PROTO files and cleanup the temporary files (#2)

--- zookeeper-recipes/zookeeper-recipes-lock/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.apache.zookeeper.recipes.lock","l":"LockListener"},{"p":"org.apache.zookeeper.recipes.lock","l":"WriteLock"},{"p":"org.apache.zookeeper.recipes.lock","l":"ZooKeeperOperation"}]
--- zookeeper-recipes/zookeeper-recipes-queue/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.apache.zookeeper.recipes.queue","l":"DistributedQueue"}]

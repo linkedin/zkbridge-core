51344c7e1c6ee48a3b57c00015e0c4dd7dcd54d5
Komal Desai <kdesai@linkedin.com>
2023-11-06 08:42:17 -0800

    Delete not required files (#4)

--- zookeeper-server/src/main/java/org/apache/zookeeper/spiral/SpiralClient.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/spiral/SpiralClient.java
@@ -4,29 +4,47 @@ import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import proto.com.linkedin.spiral.GetRequestOrBuilder;
+import proto.com.linkedin.spiral.BatchDeleteRequest;
+import proto.com.linkedin.spiral.BatchDeleteResponse;
+import proto.com.linkedin.spiral.BatchGetRequest;
+import proto.com.linkedin.spiral.BatchGetResponse;
+import proto.com.linkedin.spiral.BatchPutRequest;
+import proto.com.linkedin.spiral.BatchPutResponse;
+import proto.com.linkedin.spiral.DeleteRequest;
+import proto.com.linkedin.spiral.DeleteResponse;
+import proto.com.linkedin.spiral.GetRequest;
+import proto.com.linkedin.spiral.GetResponse;
+import proto.com.linkedin.spiral.Key;
+import proto.com.linkedin.spiral.PaginationContext;
 import proto.com.linkedin.spiral.Put;
 import proto.com.linkedin.spiral.PutRequest;
 import proto.com.linkedin.spiral.PutResponse;
+import proto.com.linkedin.spiral.ScanRequest;
+import proto.com.linkedin.spiral.ScanResponse;
 import proto.com.linkedin.spiral.SpiralApiGrpc;
 import proto.com.linkedin.spiral.SpiralContext;
-import proto.com.linkedin.spiral.Key;
-import proto.com.linkedin.spiral.GetRequest;
-import proto.com.linkedin.spiral.GetResponse;
+import proto.com.linkedin.spiral.Value;
+import proto.com.linkedin.spiral.GetRequestOrBuilder;
 import proto.com.linkedin.spiral.Value;
 
 public class SpiralClient {
   private static final Logger logger = LoggerFactory.getLogger(SpiralClient.class);
 
   private final SpiralApiGrpc.SpiralApiBlockingStub _blockingStub;
+  private final SpiralContext _spiralContext;
 
 
   public SpiralClient(String spiralEndpoint) {
     try {
       ManagedChannel channel = ManagedChannelBuilder.forTarget(spiralEndpoint).usePlaintext().build();
       _blockingStub = SpiralApiGrpc.newBlockingStub(channel);
+      _spiralContext = SpiralContext.newBuilder()
+                         .setNamespace("test")
+                         .setBucket("zk")
+                         .build();
       logger.info("Connected to spiral-service : {}", spiralEndpoint);
     } catch (Exception e) {
       logger.error("Failed to connect to spiral service at endpoint : {} {}", spiralEndpoint, e.getMessage());
@@ -35,15 +53,10 @@ public class SpiralClient {
   }
 
   public byte[] get(String key) {
-    SpiralContext cs = SpiralContext.newBuilder()
-                          .setNamespace("test")
-                          .setBucket("zk")
-                          .build();
-
-    ByteString keyBytes = ByteString.copyFromUtf8(key);
-    Key apiKey = Key.newBuilder().setMessage(keyBytes).build();
-    GetRequest request = GetRequest.newBuilder().setSpiralContext(cs).setKey(apiKey).build();
     try {
+      ByteString keyBytes = ByteString.copyFromUtf8(key);
+      Key apiKey = Key.newBuilder().setMessage(keyBytes).build();
+      GetRequest request = GetRequest.newBuilder().setSpiralContext(_spiralContext).setKey(apiKey).build();
       GetResponse response = _blockingStub.get(request);
       return response.getValue().getMessage().toByteArray();
     } catch (Exception e) {
@@ -53,20 +66,16 @@ public class SpiralClient {
   }
 
   public void put(String key, byte[] value) {
-    SpiralContext cs = SpiralContext.newBuilder()
-        .setNamespace("test")
-        .setBucket("zk")
-        .build();
-
-    ByteString keyBytes = ByteString.copyFromUtf8(key);
-    Key apiKey = Key.newBuilder().setMessage(keyBytes).build();
-    Value apiValue = Value.newBuilder().setMessage(ByteString.copyFrom(value)).build();
-    Put putValue = Put.newBuilder().setKey(apiKey).setValue(apiValue).build();
-    PutRequest request = PutRequest.newBuilder()
-        .setSpiralContext(cs)
-        .setPut(putValue)
-        .build();
     try {
+      ByteString keyBytes = ByteString.copyFromUtf8(key);
+      Key apiKey = Key.newBuilder().setMessage(keyBytes).build();
+      Value apiValue = Value.newBuilder().setMessage(ByteString.copyFrom(value)).build();
+      Put putValue = Put.newBuilder().setKey(apiKey).setValue(apiValue).build();
+      PutRequest request = PutRequest.newBuilder()
+          .setSpiralContext(_spiralContext)
+          .setPut(putValue)
+          .build();
+
       // TODO - convert response to ZK response.
       PutResponse response = _blockingStub.put(request);
     } catch (Exception e) {

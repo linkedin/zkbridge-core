bb84a7e192677cb7a11989abec7580b410602f2a
Rahul Rane <rrane@linkedin.com>
2024-03-17 23:31:27 -0700

    cleaned up code

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/util/SerializeUtils.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/util/SerializeUtils.java
@@ -264,28 +264,6 @@ public class SerializeUtils {
         dt.serialize(oa, "tree");
     }
 
-    // public static byte[] serializeToByteArray(final Object obj) {
-    //     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-    //     try (ObjectOutputStream out = new ObjectOutputStream(bos)) {
-    //         out.writeObject(obj);
-    //         out.flush();
-    //         return bos.toByteArray();
-    //     } catch (Exception ex) {
-    //         throw new RuntimeException(ex);
-    //     }
-    // }
-
-    // public static Object deserializeFromByteArray(byte[] bytes) {
-    //     ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-
-    //     try (ObjectInput in = new ObjectInputStream(bis)) {
-    //         return in.readObject();
-    //     } catch (Exception ex) {
-    //         throw new RuntimeException(ex);
-    //     }
-    // }
-
     public static byte[] serializeToByteArray(DataNode node) throws IOException {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         BinaryOutputArchive boa = BinaryOutputArchive.getArchive(bos);
